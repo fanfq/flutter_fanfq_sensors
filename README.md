47358515d8b67d55ed440cbb8510dc61002c568b
fred <fangqing.fan@hotmail.com>
2022-06-17 13:29:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_fanfq_sensors
+
+陀螺仪传感器
+
+- [pub.dev: sensors_plus](https://pub.dev/packages/sensors_plus)
---
+++ android/app/src/main/kotlin/com/example/flutter_fanfq_sensors/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fanfq_sensors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
